5c03e83d9c517453ea671ae9de2aa7f8623c02ab
azizah <baitulazizah12@gmail.com>
2024-09-03 15:13:50 +0700

    alhamdulillah bagian user done

--- application/views/user_home.php
+++ application/views/user_home.php
@@ -1,95 +1,48 @@
 <div class="container-fluid"> 
-
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800"> Home</h1>
-                        
-                        <!-- <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-plus"></i>      Tambah data</button> -->
                     </div>
-                    
-                    <!-- DataTales Example -->
-                    <div class="card shadow mb-4">
-                    <div class="card-header py-3 d-flex justify-content-between align-items-center">
-                    <h6 class="m-0 font-weight-bold text-success">
-        <i class="fas fa-fw fa-file-alt text-success"></i> Data Permohonan
-    </h6>
-    <button class="btn btn-primary btn-sm">
-         Ajukan Permohonan
-    </button>
-   
-</div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="10" >
-                                    <thead class="text-center bg-success text-white">
-                                        <tr>
-                                            <th>No</th>
-                                            <th>Nama Usaha</th>
-                                            <th>Status</th>
-                                            <th>Detail Permohonan</th>
-                                        </tr>
-                                       
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td class= "text-center">1</td>
-                                            <td>Usaha</td>
-                                            <td class= ><div class="status-containe text-center">
-                                                <div class="status diterima">Diterima</div></div>
-                                            </td>
-                                            <td class="text-center">
-                                                <button type="submit" class="btn btn-info btn-sm">Berkas</button>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td class= "text-center">1</td>
-                                            <td>Usaha</td>
-                                            <td class= ><div class="status-containe text-center">
-                                                <div class="status diproses">Diproses</div></div>
-                                            </td>
-                                            <td class="text-center">
-                                                <button type="submit" class="btn btn-info btn-sm">Berkas</button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                
+                   <div class="row">
+                        <div class="col-xl-8 col-lg-7">
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-success">Petunjuk Pengajuan Permohonan</h6>
+                                </div>
+                                <div class="card-body">
+                                    <ol>
+                                        <li>Siapkan berkas persyaratan yang dibutuhkan:
+                                            <ul>
+                                                <li>Proposal permohonan bantuan (format pdf)</li>
+                                                <li>Surat Keterangan Usaha yang diketahui oleh Wali Nagari (format pdf)</li>
+                                                <li>Foto Kartu Keluarga (format pdf, png, jpg, atau jpeg)</li>
+                                                <li>Foto KTP (format pdf, png, jpg, atau jpeg)</li>
+                                            </ul>
+                                        </li>
+                                        <li>Ajukan permohonan pada menu "Permohonan" dan silakan isi form sesuai dengan data yang diminta</li>
+                                        <li>Pastikan semua data yang diisi dalam formulir adalah benar dan sesuai dengan dokumen resmi.</li>
+                                        <li>Unggah berkas pendukung seperti Proposal, Surat Keterangan Usaha (SKU), Foto Kartu Keluarga (KK), dan Foto KTP.</li>
+                                        <li>Jika semua data sudah benar, klik tombol "Simpan" untuk mengirim permohonan.</li>
+                                        <li>Anda dapat memantau status permohonan secara berkala.</li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-xl-4 col-lg-5">
+                            <div class="card shadow mb-4">
+                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-success">Template Berkas Permohonan</h6>
+                                </div>
+                                <div class="card-body">
+                                <p>Di bawah ini adalah template proposal yang dapat Anda gunakan untuk pengajuan permohonan:</p>
+                                    <!-- Tautan untuk mengunduh template -->
+                                    <a href="<?= base_url('assets/templates/template_proposal.docx'); ?>" class="btn btn-info btn-sm">
+                                        Unduh Template Proposal
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <!-- <!DOCTYPE html> -->
-<!-- <html lang="en">
 
-    <style>
-         .status {
-            display: inline-block;
-            padding: 5px 12px;
-            margin: 0px;
-            font-size: 12px;
-            border-radius: 10px;
-            color: #fff;
-            text-align: center;
-            font-family: inherit;
-        }
-        .status.diterima {
-            background-color: #28a745;
-        }
-        .status.ditolak {
-            background-color: #dc3545;
-        }
-        .status.diproses {
-            background-color: #ffc107;
-            color: #000;
-        }
-    </style>
-
-<body>
-    <div class="status-container">
-        <div class="status diterima">Diterima</div>
-        <div class="status ditolak">Ditolak</div>
-        <div class="status diproses">Diproses</div>
-    </div>
-</body>
-</html> -->
-<!-- 
-                </div> -->
-                <!-- /.container-fluid -->
